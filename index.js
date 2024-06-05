6ad4db47dd4daf69434d8034326d8520fca6c221
yeasinarafat51 <aloondokar65@gmail.com>
2024-06-05 22:05:07 +0600

    problem

--- index.js
+++ index.js
@@ -44,102 +44,95 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
+const dbConnect = async () => {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    const productDB = client.db("productDB");
-    const userDB = client.db("userDB");
+      client.connect();
+      console.log("Database Connected Successfully✅");
 
-    const shoesCollection = productDB.collection("shoesCollection");
-    const userCollection = userDB.collection("userCollection");
-    // product
-    app.post("/shoes",verifyToken,  async (req, res) => {
-      const shoesData = req.body;
-      const result = await shoesCollection.insertOne(shoesData);
-      res.send(result);
-    });
-    app.get("/shoes", async (req, res) => {
-      const shoesData = shoesCollection.find();
-      const result = await shoesData.toArray();
-      res.send(result);
-    });
-    app.get("/shoes/:id", async (req, res) => {
-      const id = req.params.id;
-      const shoesData = await shoesCollection.findOne({
-        _id: new ObjectId(id),
-      });
-      res.send(shoesData);
-    });
-    app.patch("/shoes/:id",verifyToken,  async (req, res) => {
-      const id = req.params.id;
-      const updatedData = req.body;
-      const result = await shoesCollection.updateOne(
-        { _id: new ObjectId(id) },
-        { $set: updatedData }
-      );
-      res.send(result);
-    });
-    app.delete("/shoes/:id", verifyToken, async (req, res) => {
-      const id = req.params.id;
-      const result = await shoesCollection.deleteOne({ _id: new ObjectId(id) });
-      res.send(result);
-    });
-
-     // user
-     app.post("/user",  async (req, res) => {
-      const user = req.body;
-
-      const token = createToken(user);
-      const isUserExist = await userCollection.findOne({ email: user?.email });
-      if (isUserExist?._id) {
-        return res.send({
-          statu: "success",
-          message: "Login success",
-          token
-          
-        });
-      }
-     await userCollection.insertOne(user);
-      return res.send({token});
-    });
-    app.get("/user/get/:id", async (req, res) => {
-      const id = req.params.id;
-      console.log(id);
-      const result = await userCollection.findOne({ _id: new ObjectId(id) });
-      res.send(result);
-    });
-
-    app.get("/user/:email", async (req, res) => {
-      const email = req.params.email;
-      console.log(email)
-      const result = await userCollection.findOne({ email });
-      res.send(result);
-    });
-
-    app.patch("/user/:email", async (req, res) => {
-      const email = req.params.email;
-      const userData = req.body;
-      const result = await userCollection.updateOne(
-        { email },
-        { $set: userData },
-        { upsert: true }
-      );
-      console.log(result)
-      res.send(result);
-
-    });
-
-
-    // Send a ping to confirm a successful connection
-   
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    
+  } catch (error) {
+      console.log(error.name, error.message);
   }
 }
-run().catch(console.dir);
+dbConnect()
+
+const shoesCollection = client.db('productDB').collection('shoesCollection')
+const userCollection = client.db('userDB').collection('userCollection')
+
+app.post("/shoes",verifyToken,  async (req, res) => {
+  const shoesData = req.body;
+  const result = await shoesCollection.insertOne(shoesData);
+  res.send(result);
+});
+app.get("/shoes", async (req, res) => {
+  const shoesData = shoesCollection.find();
+  const result = await shoesData.toArray();
+  res.send(result);
+});
+app.get("/shoes/:id", async (req, res) => {
+  const id = req.params.id;
+  const shoesData = await shoesCollection.findOne({
+    _id: new ObjectId(id),
+  });
+  res.send(shoesData);
+});
+app.patch("/shoes/:id",verifyToken,  async (req, res) => {
+  const id = req.params.id;
+  const updatedData = req.body;
+  const result = await shoesCollection.updateOne(
+    { _id: new ObjectId(id) },
+    { $set: updatedData }
+  );
+  res.send(result);
+});
+app.delete("/shoes/:id", verifyToken, async (req, res) => {
+  const id = req.params.id;
+  const result = await shoesCollection.deleteOne({ _id: new ObjectId(id) });
+  res.send(result);
+});
+
+ // user
+ app.post("/user",  async (req, res) => {
+  const user = req.body;
+
+  const token = createToken(user);
+  const isUserExist = await userCollection.findOne({ email: user?.email });
+  if (isUserExist?._id) {
+    return res.send({
+      statu: "success",
+      message: "Login success",
+      token
+      
+    });
+  }
+ await userCollection.insertOne(user);
+  return res.send({token});
+});
+app.get("/user/get/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const result = await userCollection.findOne({ _id: new ObjectId(id) });
+  res.send(result);
+});
+
+app.get("/user/:email", async (req, res) => {
+  const email = req.params.email;
+  console.log(email)
+  const result = await userCollection.findOne({ email });
+  res.send(result);
+});
+
+app.patch("/user/:email", async (req, res) => {
+  const email = req.params.email;
+  const userData = req.body;
+  const result = await userCollection.updateOne(
+    { email },
+    { $set: userData },
+    { upsert: true }
+  );
+  console.log(result)
+  res.send(result);
+
+});
 
 
 
