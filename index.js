5f430beb3cfd5da6134f135860873019474596a3
yeasinarafat51 <aloondokar65@gmail.com>
2024-06-05 20:07:38 +0600

    server1

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function verifyToken(req, res, next) {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v58lgaw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient with aa MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
